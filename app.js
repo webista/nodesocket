3f327b948740e8a8028d0ddb0e11640a3720a5ea
Barry Lavides <lavidesbarry@gmail.com>
2013-10-22 22:43:24 +0800

    updating disconnected users is working already

--- app.js
+++ app.js
@@ -18,11 +18,21 @@ io.sockets.on('connection', function(socket) {
       callback(true);
       socket.nickname = data;
       nicknames.push(socket.nickname);
-      io.sockets.emit('usernames', nicknames) ;
+      updateNicknames();
     }
   });
+
+  function updateNicknames() {
+    io.sockets.emit('usernames', nicknames);
+  };
   
   socket.on('send message', function(data) {
     io.sockets.emit('new message', data);
+  });
+
+  socket.on('disconnect', function(data) {
+    if (!socket.nickname) return;
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+    updateNicknames();
   });
 });
