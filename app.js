4b68411b40bafd5f89281e4d88fd26297781af4f
Barry Lavides <lavidesbarry@gmail.com>
2013-11-11 15:41:20 +0800

    private msging is working already[D

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function(socket) {
     socket.on('new user', function(data, callback) {
         if (data in users) {
+            // If users exists
             callback(false);
         } else {
             callback(true);
             socket.nickname = data;
             users[socket.nickname] = socket;
-            nicknames.push(socket.nickname);
             updateNicknames();
         }
     });
@@ -27,12 +27,37 @@ io.sockets.on('connection', function(socket) {
         io.sockets.emit('usernames', Object.keys(users));
     };
   
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+    socket.on('send message', function(data, callback) {
+        var msg = data.trim();
+
+        if (msg.substr(0,3) === '/w ') {
+            msg = msg.substr(3);
+
+            var ind = msg.indexOf(' ');
+
+            if (ind !== -1) {
+                var name = msg.substring(0, ind),
+                    msg = msg.substring(ind + 1);
+
+                if (name in users) {
+                    users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+                    console.log('whisper');                    
+                } else {
+                    callback('Error! Enter a valid user.');
+                }
+            } else {
+                callback('Error! Please enter a message for your whisper.');
+            }
+        } else {
+            io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+        }
     });
 
     socket.on('disconnect', function(data) {
-        if (!socket.nickname) return;
+        if (!socket.nickname) {
+            return;
+        }
+
         delete users[socket.nickname];
         updateNicknames();
     });
