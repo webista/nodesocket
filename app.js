fec77a70c1c306a0d8b0b6fc6b1bf03736dd0d8e
Barry Lavides <lavidesbarry@gmail.com>
2013-11-11 14:07:40 +0800

    to 4spaces tabs

--- app.js
+++ app.js
@@ -2,37 +2,38 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    nicknames = [];
+    users = {};
 
 server.listen(3000);
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket) {
-  socket.on('new user', function(data, callback) {
-    if (nicknames.indexOf(data) != -1) {
-      callback(false);
-    } else {
-      callback(true);
-      socket.nickname = data;
-      nicknames.push(socket.nickname);
-      updateNicknames();
-    }
-  });
+    socket.on('new user', function(data, callback) {
+        if (data in users) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            users[socket.nickname] = socket;
+            nicknames.push(socket.nickname);
+            updateNicknames();
+        }
+    });
 
-  function updateNicknames() {
-    io.sockets.emit('usernames', nicknames);
-  };
+    function updateNicknames() {
+        io.sockets.emit('usernames', Object.keys(users));
+    };
   
-  socket.on('send message', function(data) {
-    io.sockets.emit('new message', {msg: data, nick: socket.nickname});
-  });
+    socket.on('send message', function(data) {
+        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+    });
 
-  socket.on('disconnect', function(data) {
-    if (!socket.nickname) return;
-    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-    updateNicknames();
-  });
+    socket.on('disconnect', function(data) {
+        if (!socket.nickname) return;
+        delete users[socket.nickname];
+        updateNicknames();
+    });
 });
